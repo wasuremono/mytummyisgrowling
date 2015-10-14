98b1f8e85bd3a58f52488a086eb75eac1ac3ed11
Andrew Lee <andrewlee51285@gmail.com>
2015-10-14 21:17:09 +1100

    Added custom InfoWindow to the markers in MapsFromListActivity map screen.

--- app/src/main/java/comp4920/mytummyisgrowling/MapsFromListActivity.java
+++ app/src/main/java/comp4920/mytummyisgrowling/MapsFromListActivity.java
@@ -3,6 +3,9 @@ package comp4920.mytummyisgrowling;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +16,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,8 @@ import comp4920.mytummyisgrowling.yelp.YelpAPI;
 
 public class MapsFromListActivity extends FragmentActivity {
 
+
+
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private static LatLng NEWARK;
@@ -28,6 +34,68 @@ public class MapsFromListActivity extends FragmentActivity {
     private CameraPosition POSITION;
 
     private ArrayList<Business> myBusinessList;
+
+
+    class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+        private final View myContentsView;
+
+        MyInfoWindowAdapter() {
+            myContentsView = getLayoutInflater().inflate(R.layout.map_marker_window, null);
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+
+            int indexInt = 0;
+            Business business = null;
+
+            String snippet = marker.getSnippet();
+
+
+            if(!snippet.equals("This is where you are.")) {
+                indexInt = Integer.parseInt(marker.getSnippet());
+                business = myBusinessList.get(indexInt);
+
+                ImageView image = (ImageView) myContentsView.findViewById(R.id.mapWindowImage);
+                String imageURL = business.getImage_url();
+                Picasso.with(getMapsFromListActivity()).load(imageURL).into(image);
+
+
+                TextView name = ((TextView)myContentsView.findViewById(R.id.mapWindowName));
+                name.setText(business.getName());
+
+                StringBuffer addressString = new StringBuffer("");
+                for(String string : business.getLocation().getDisplay_address()) {
+                    addressString.append(string + "\n");
+                }
+                TextView address = ((TextView)myContentsView.findViewById(R.id.mapWindowAddress));
+                address.setText(addressString);
+
+            } else {
+                TextView name = ((TextView)myContentsView.findViewById(R.id.mapWindowName));
+                name.setText("Your current location");
+
+                TextView address = ((TextView)myContentsView.findViewById(R.id.mapWindowAddress));
+                address.setText("You are currently here!");
+
+            }
+
+
+
+
+            return myContentsView;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
+
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,23 +180,38 @@ public class MapsFromListActivity extends FragmentActivity {
         mUiSettings.setZoomGesturesEnabled(true);
         mUiSettings.setTiltGesturesEnabled(false);
         mUiSettings.setRotateGesturesEnabled(true);
+
+
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(POSITION));
 
+
         Marker currLocation = mMap.addMarker(new MarkerOptions().position(NEWARK)
-                        .title("CURR LOCATION")
+                        .title("Current Location!")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                        .snippet("This is where you are.")
         );
+
+
+        mMap.setInfoWindowAdapter(new MyInfoWindowAdapter());
 
         for(int i = 0; i < myBusinessList.size(); i++) {
             Business currBus = myBusinessList.get(i);
             String currBusName = currBus.getName();
             LatLng currBusLatLng = new LatLng(currBus.getLocation().getCoordinate().getLatitude(),
                                               currBus.getLocation().getCoordinate().getLongitude());
-            Marker businessMarker = mMap.addMarker(new MarkerOptions().position(currBusLatLng).title(currBusName));
+            String indexString = Integer.toString(i).trim();
+            System.out.println("indexString is: " + indexString);
+            Marker businessMarker = mMap.addMarker(new MarkerOptions().position(currBusLatLng).title(currBusName).snippet(indexString));
         }
 
 
 
 
     }
+
+    public MapsFromListActivity getMapsFromListActivity() {
+        return MapsFromListActivity.this;
+    }
+
+
 }
