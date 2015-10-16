ee6ab5ce46c8c21f7099fb7e856b2de6188e5678
Matthew Leung-Harrison <mlxtreme@gmail.com>
2015-10-16 14:52:15 +1100

    Login Screen Stuff
    Added a simple login screen.

---
+++ app/src/main/java/comp4920/mytummyisgrowling/HomeActivity.java
@@ -0,0 +1,23 @@
+package comp4920.mytummyisgrowling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void login (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
